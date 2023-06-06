bfe0e1c064ca6576a4636172d3595f912697a850
gioboa <giorgiob.boa@gmail.com>
2023-06-06 08:35:21 +0200

    add SfProgressCircular

---
+++ packages/storefront-ui/src/components/SfProgressCircular/types.ts
@@ -0,0 +1,8 @@
+import { SfProgressSize } from '../../shared/SfProgress';
+
+export type SfProgressCircularProps = {
+  class?: string;
+  value?: number;
+  size?: `${SfProgressSize}`;
+  ariaLabel?: string;
+};
