4cf374b4319cd9f0207000868dfd40dae7ced589
gioboa <giorgiob.boa@gmail.com>
2023-06-06 08:33:03 +0200

    add SfCheckbox

---
+++ packages/storefront-ui/src/components/SfCheckbox/types.ts
@@ -0,0 +1,9 @@
+import { QwikIntrinsicElements, Signal } from '@builder.io/qwik';
+
+export type SfCheckboxProps = Omit<
+  QwikIntrinsicElements['input'],
+  'children'
+> & {
+  ref?: Signal<Element | undefined>;
+  invalid?: boolean;
+};
