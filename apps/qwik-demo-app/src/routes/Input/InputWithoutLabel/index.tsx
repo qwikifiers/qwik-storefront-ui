aa269caac29cb19dbdc0bb9f30c19130aefdcb7b
gioboa <giorgiob.boa@gmail.com>
2023-06-06 11:06:15 +0200

    add Input example

---
+++ apps/qwik-demo-app/src/routes/Input/InputWithoutLabel/index.tsx
@@ -0,0 +1,12 @@
+import { component$ } from '@builder.io/qwik';
+import { SfInput } from 'qwik-storefront-ui';
+
+export default component$(() => {
+  return (
+    <div class="flex flex-col gap-y-5">
+      <SfInput size="sm" aria-label="Label size sm" />
+      <SfInput aria-label="Label size base" />
+      <SfInput size="lg" aria-label="Label size lg" />
+    </div>
+  );
+});
