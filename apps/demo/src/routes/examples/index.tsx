f36148a30d7b64de494cfa044c4c57b342bfeee1
gioboa <giorgiob.boa@gmail.com>
2023-09-19 18:29:38 +0200

    remove demo site

--- apps/demo/src/routes/examples/index.tsx
+++
@@ -1,5 +0,0 @@
-import type { RequestHandler } from '@builder.io/qwik-city';
-
-export const onRequest: RequestHandler = async ({ redirect }) => {
-  throw redirect(308, '/examples/SfAccordionItem/');
-};
