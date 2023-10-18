d41a21e9a53dd3e1aa9c7f24bdd2dc6b3c76a280
gioboa <giorgiob.boa@gmail.com>
2023-10-18 22:45:01 +0200

    Revert "chore: change vite.config"
    This reverts commit 359bec6067d76ff630db970999463f0c172a3764.

--- packages/qwik-storefront-ui/vite.config.ts
+++ packages/qwik-storefront-ui/vite.config.ts
@@ -1,19 +1,25 @@
 /// <reference types="vitest" />
 
 import { qwikVite } from '@builder.io/qwik/optimizer';
-import { join } from 'path';
+import { dirname, join } from 'path';
+import { qwikNxVite } from 'qwik-nx/plugins';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [
-    qwikVite({
-      vendorRoots: [join(__dirname, '../qwik-storefront-ui/src')],
-    }),
-    tsconfigPaths(),
+    qwikNxVite(),
+    qwikVite(),
+    tsconfigPaths({ root: '../../' }),
     dts({
-      tsonfigPath: join(__dirname, 'tsconfig.lib.json'),
+      tsConfigFilePath: join(
+        dirname(fileURLToPath(import.meta.url)),
+        'tsconfig.lib.json'
+      ),
+      // Faster builds by skipping tests. Set this to false to enable type checking.
+      skipDiagnostics: true,
     }),
   ],
   server: {
@@ -22,15 +28,19 @@ export default defineConfig({
       allow: ['../../'],
     },
   },
-  mode: 'lib',
+
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
+    target: 'es2020',
+    emptyOutDir: true,
     lib: {
+      entry: './src/index.ts',
+
       // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
       name: 'qwik-storefront-ui',
       fileName: (format) => `index.qwik.${format === 'es' ? 'mjs' : 'cjs'}`,
+      // fileName: 'index',
       // Change this to the formats you want to support.
       // Don't forgot to update your package.json as well.
       formats: ['es', 'cjs'],
